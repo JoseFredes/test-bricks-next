c1acf8e17881fea8f8e12c3653d6a16b11d2e656
JoseFredes <jose.fredes@mayor.cl>
2024-12-31 17:49:24 -0300

    Refactor page component: add hero section in homeBricks

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect } from "react";
 import { JSONComponent, renderComponentFromJSON } from "jbricks";
 import { navbar } from "./components/homeBricks/navbar";
+import { heroSection } from "./components/homeBricks/heroSection";
 
 const Page = () => {
   useEffect(() => {
@@ -21,48 +22,7 @@ const Page = () => {
         // Navbar
         navbar,
         // Hero Section
-        {
-          type: "section",
-          props: {
-            className: "hero",
-            style: {
-              textAlign: "center",
-              padding: "50px 20px",
-              backgroundColor: "#e0f7fa",
-            },
-          },
-          children: [
-            {
-              type: "h1",
-              props: { style: { fontSize: "36px", color: "#00796b" } },
-              children: ["Bienvenido a JBricks"],
-            },
-            {
-              type: "p",
-              props: { style: { fontSize: "18px", color: "#555" } },
-              children: [
-                "La solución para renderizar componentes dinámicamente desde JSON.",
-              ],
-            },
-            {
-              type: "a",
-              props: {
-                href: "https://npmjs.com/package/jbricks",
-                style: {
-                  display: "inline-block",
-                  marginTop: "20px",
-                  padding: "10px 20px",
-                  backgroundColor: "#00796b",
-                  color: "#fff",
-                  textDecoration: "none",
-                  borderRadius: "5px",
-                  boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-                },
-              },
-              children: ["Descargar en npm"],
-            },
-          ],
-        },
+        heroSection,
         // Features Section
         {
           type: "section",
