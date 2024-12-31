d6e12d85a1c23fb86f40875f47eadcfa5a4be9a8
JoseFredes <jose.fredes@mayor.cl>
2024-12-31 18:36:21 -0300

    Refactor homeBricks footerSection component: Add footerSection component in homeBricks

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { heroSection } from "./components/homeBricks/heroSection";
 import { featuresSection } from "./components/homeBricks/featuresSection";
 import { aboutSection } from "./components/homeBricks/aboutSection";
 import { contactSection } from "./components/homeBricks/contactSection";
+import { footerSection } from "./components/homeBricks/footerSection";
 
 const Page = () => {
   useEffect(() => {
@@ -33,68 +34,7 @@ const Page = () => {
         // Contact Section
         contactSection,
         // Footer
-        {
-          type: "footer",
-          props: {
-            style: {
-              padding: "20px",
-              backgroundColor: "#333",
-              color: "#fff",
-              textAlign: "center",
-              marginTop: "20px",
-            },
-          },
-          children: [
-            {
-              type: "div",
-              props: {
-                style: {
-                  display: "flex",
-                  marginBottom: "10px",
-                  marginTop: "10px",
-                  justifyContent: "center",
-                  gap: "20px",
-                },
-              },
-              children: [
-                {
-                  type: "p",
-                  children: [
-                    {
-                      type: "span",
-                      children: ["Hecho con ❤️ por "],
-                    },
-                    {
-                      type: "a",
-                      props: {
-                        href: "https://github.com/JoseFredes",
-                        style: { color: "#fff", textDecoration: "none" },
-                      },
-                      children: ["@JoseFredes"],
-                    },
-                  ],
-                },
-                {
-                  type: "p",
-                  children: [
-                    {
-                      type: "a",
-                      props: {
-                        href: "https://www.linkedin.com/in/josefredesacevedo/",
-                        style: {
-                          color: "#fff",
-                          textDecoration: "none",
-                          marginTop: "10px",
-                        },
-                      },
-                      children: ["LinkenIn"],
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
+        footerSection,
       ],
     };
 
