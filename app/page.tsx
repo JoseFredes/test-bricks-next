ef560f7debcf8385bb42b3f7eac289fb17a7c6c2
JoseFredes <jose.fredes@mayor.cl>
2024-12-31 17:52:36 -0300

    Refactor page component: add about section in homeBricks

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { JSONComponent, renderComponentFromJSON } from "jbricks";
 import { navbar } from "./components/homeBricks/navbar";
 import { heroSection } from "./components/homeBricks/heroSection";
 import { featuresSection } from "./components/homeBricks/featuresSection";
+import { aboutSection } from "./components/homeBricks/aboutSection";
 
 const Page = () => {
   useEffect(() => {
@@ -27,59 +28,7 @@ const Page = () => {
         // Features Section
         featuresSection,
         // About Section
-        {
-          type: "section",
-          props: {
-            id: "about",
-            style: { padding: "40px 20px", backgroundColor: "#f5f5f5" },
-          },
-          children: [
-            {
-              type: "h2",
-              props: { style: { fontSize: "28px", color: "#333" } },
-              children: [
-                {
-                  type: "span",
-                  children: ["Acerca de JBricks"],
-                },
-                {
-                  type: "span",
-                  props: { style: { fontSize: "16px", color: "#555" } },
-                  children: [" (v1.0.0)"],
-                },
-                {
-                  type: "h3",
-                  props: { style: { fontSize: "20px", color: "#00796b" } },
-                  children: ["Mira los ejemplos:"],
-                },
-                {
-                  type: "button",
-                  props: {
-                    style: {
-                      padding: "10px 20px",
-                      backgroundColor: "#00796b",
-                      color: "#fff",
-                      border: "none",
-                      borderRadius: "5px",
-                      cursor: "pointer",
-                    },
-                    onClick: () => (window.location.href = "/forms"),
-                  },
-                  children: ["Ver ejemplos de formularios"],
-                },
-              ],
-            },
-            {
-              type: "p",
-              props: {
-                style: { fontSize: "16px", color: "#555", marginTop: "10px" },
-              },
-              children: [
-                "JBricks es una librería pensada para desarrolladores modernos que buscan flexibilidad en la creación de interfaces dinámicas.",
-              ],
-            },
-          ],
-        },
+        aboutSection,
         // Contact Section
         {
           type: "section",
