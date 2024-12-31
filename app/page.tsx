71216de0f7734ddbbce24c35232dcda7f5678972
JoseFredes <jose.fredes@mayor.cl>
2024-12-31 04:17:45 -0300

    Refactor footer styling in page component

--- app/page.tsx
+++ app/page.tsx
@@ -296,32 +296,50 @@ const Page = () => {
           },
           children: [
             {
-              type: "p",
+              type: "div",
+              props: {
+                style: {
+                  display: "flex",
+                  marginBottom: "10px",
+                  marginTop: "10px",
+                  justifyContent: "center",
+                  gap: "20px",
+                },
+              },
               children: [
                 {
-                  type: "span",
-                  children: ["Hecho con ❤️ por "],
+                  type: "p",
+                  children: [
+                    {
+                      type: "span",
+                      children: ["Hecho con ❤️ por "],
+                    },
+                    {
+                      type: "a",
+                      props: {
+                        href: "https://github.com/JoseFredes",
+                        style: { color: "#fff", textDecoration: "none" },
+                      },
+                      children: ["@JoseFredes"],
+                    },
+                  ],
                 },
                 {
-                  type: "a",
-                  props: {
-                    href: "https://github.com/JoseFredes",
-                    style: { color: "#fff", textDecoration: "none" },
-                  },
-                  children: ["@JoseFredes"],
-                },
-              ],
-            },
-            {
-              type: "p",
-              children: [
-                {
-                  type: "a",
-                  props: {
-                    href: "https://www.linkedin.com/in/josefredesacevedo/",
-                    style: { color: "#fff", textDecoration: "none" },
-                  },
-                  children: ["LinkenIn"],
+                  type: "p",
+                  children: [
+                    {
+                      type: "a",
+                      props: {
+                        href: "https://www.linkedin.com/in/josefredesacevedo/",
+                        style: {
+                          color: "#fff",
+                          textDecoration: "none",
+                          marginTop: "10px",
+                        },
+                      },
+                      children: ["LinkenIn"],
+                    },
+                  ],
                 },
               ],
             },
