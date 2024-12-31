21ec4759188ab0414d416006036d59933a4f397d
JoseFredes <jose.fredes@mayor.cl>
2024-12-31 03:57:28 -0300

    Refactor form styles in page component: Add new heading element for examples

--- app/page.tsx
+++ app/page.tsx
@@ -169,6 +169,11 @@ const Page = () => {
                   props: { style: { fontSize: "16px", color: "#555" } },
                   children: [" (v1.0.0)"],
                 },
+                {
+                  type: "h3",
+                  props: { style: { fontSize: "20px", color: "#00796b" } },
+                  children: ["Mira los ejemplos:"],
+                },
               ],
             },
             {
