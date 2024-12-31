61a2e92de2e6c7b9e0ceeabb3f3d6f94e5ac52b9
JoseFredes <jose.fredes@mayor.cl>
2024-12-31 19:38:25 -0300

    Refactor hello component: Update import of React and add JSX type import

--- app/react/components/hello.tsx
+++ app/react/components/hello.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { JSX, useEffect, useState } from "react";
 import { JSONComponent, renderComponentFromJSON } from "jbricks";
 
 const json: JSONComponent = {
