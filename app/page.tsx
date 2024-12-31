177e47c82835959db83d8f52cea54c022a1cd919
JoseFredes <jose.fredes@mayor.cl>
2024-12-31 17:50:53 -0300

    Refactor page component: add features section in homeBricks

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from "react";
 import { JSONComponent, renderComponentFromJSON } from "jbricks";
 import { navbar } from "./components/homeBricks/navbar";
 import { heroSection } from "./components/homeBricks/heroSection";
+import { featuresSection } from "./components/homeBricks/featuresSection";
 
 const Page = () => {
   useEffect(() => {
@@ -24,40 +25,7 @@ const Page = () => {
         // Hero Section
         heroSection,
         // Features Section
-        {
-          type: "section",
-          props: {
-            id: "features",
-            style: {
-              padding: "40px 20px",
-              backgroundColor: "#fff",
-              boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-            },
-          },
-          children: [
-            {
-              type: "h2",
-              props: { style: { fontSize: "28px", color: "#333" } },
-              children: ["Características"],
-            },
-            {
-              type: "ul",
-              props: {
-                style: {
-                  margin: "20px 0",
-                  padding: "0 20px",
-                  listStyle: "circle",
-                  color: "#555",
-                },
-              },
-              children: [
-                { type: "li", children: ["Fácil de usar."] },
-                { type: "li", children: ["Agnóstico al framework."] },
-                { type: "li", children: ["Altamente extensible."] },
-              ],
-            },
-          ],
-        },
+        featuresSection,
         // About Section
         {
           type: "section",
